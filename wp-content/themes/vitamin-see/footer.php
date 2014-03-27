e539d2525ab575153fad5523e53e79c9160a6581
yanai <edriyanai@gmail.com>
2014-03-27 09:23:12 +0200

    all

---
+++ wp-content/themes/vitamin-see/footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for Footer.
+ *
+ * @package Simon WP Framework
+ * @since Simon WP Framework 1.0
+ */
+?>
+
+<?php /*?><div style="clear: both"></div>
+<div class="outer_wrap">
+  <div class="inner_wrap">
+    <div class="content">
+      <div style="clear: both"></div>
+      <div id="footer">
+        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer1") ) : ?>
+        <?php endif; ?>
+        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer2") ) : ?>
+        <?php endif; ?>
+        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer3") ) : ?>
+        <?php endif; ?>
+        <div style="clear: both"></div>
+    </div>
+  </div>
+</div><?php */?>
+<?php wp_footer(); ?>
+</body></html>
